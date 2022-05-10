d3c4505b9aed17181b9f99b8920d2fba437ce649
Kriss-Kross33 <kwamefosu33@gmail.com>
2022-05-10 12:57:35 +0000

    wrap widgets with  for responsiveness across devices

--- lib/src/features/home/widgets/circular_highway_container.dart
+++ lib/src/features/home/widgets/circular_highway_container.dart
@@ -1,4 +1,5 @@
 import 'package:car_rental/src/core/constants/asset_consts.dart';
+import 'package:car_rental/src/core/themes/theme.dart';
 import 'package:car_rental/src/responsive_layout.dart';
 import 'package:flutter/material.dart';
 
@@ -7,22 +8,15 @@ class CircularHighwayContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ResponsivePadding(
-      phonePadding: const EdgeInsets.only(right: 50, top: 150),
-      tabletPadding: const EdgeInsets.only(right: 260, bottom: 70),
-      destopPadding: const EdgeInsets.only(right: 260, bottom: 70),
-      child: Align(
-        alignment: Alignment.centerRight,
-        child: ResponsiveLayout(
-          phone: CircularImageStack(
-            size: 300,
-          ),
-          tablet: CircularImageStack(
-            size: 340,
-          ),
-          desktop: CircularImageStack(
-            size: 540,
-          ),
+    return Align(
+      alignment: Alignment.centerRight,
+      child: ResponsiveLayout(
+        phone: CircularImageStack(
+          size: 300,
+        ),
+        tablet: Container(),
+        desktop: CircularImageStack(
+          size: 600,
         ),
       ),
     );
@@ -39,37 +33,42 @@ class CircularImageStack extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(size / 2),
-      child: Stack(
-        children: [
-          Container(
-            height: size,
-            width: size,
-            child: Image.asset(
-              AssetConsts.highway2,
-              fit: BoxFit.cover,
-            ),
-          ),
-          Container(
-            height: size,
-            width: size,
-            decoration: BoxDecoration(
-              gradient: LinearGradient(
-                begin: FractionalOffset.topCenter,
-                end: FractionalOffset.bottomCenter,
-                colors: [
-                  Color(0xfff73f1c).withOpacity(0.6),
-                  Color(0xfff73f1c).withOpacity(0.9),
-                  Color(0xfff73f1c).withOpacity(0.8),
-                  //Theme.of(context).colorScheme.secondary,
-                  // Theme.of(context).colorScheme.secondary
-                ],
-                stops: [0.0, 1.0, 1.0],
+    return ResponsivePadding(
+      phonePadding: const EdgeInsets.only(right: 50, top: 150),
+      tabletPadding: const EdgeInsets.only(right: 260, bottom: 70),
+      destopPadding: const EdgeInsets.only(right: 260, bottom: 70),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(size / 2),
+        child: Stack(
+          children: [
+            Container(
+              height: size,
+              width: size,
+              child: Image.asset(
+                AssetConsts.highway2,
+                fit: BoxFit.cover,
               ),
             ),
-          ),
-        ],
+            Container(
+              height: size,
+              width: size,
+              decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: FractionalOffset.topCenter,
+                  end: FractionalOffset.bottomCenter,
+                  colors: [
+                    AppColor.deepOrange.withOpacity(0.7),
+                    AppColor.deepOrange.withOpacity(0.9),
+                    AppColor.deepOrange.withOpacity(0.9),
+                    //Theme.of(context).colorScheme.secondary,
+                    // Theme.of(context).colorScheme.secondary
+                  ],
+                  stops: [0.0, 1.0, 1.0],
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
